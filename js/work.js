c88af1ecc57a0345e2f4521e37cf3807c366dbbe
sawasick <50393229+sawasick@users.noreply.github.com>
2021-02-28 22:15:50 +0900

    #16;update:Pileのキャプションを変更

--- js/work.js
+++ js/work.js
@@ -1,7 +1,7 @@
 //------slick-----
 if (window.innerWidth <= 959) {
 	//スマホ,タブレット表示
-	$(".detail_img-slide").slick({
+	$('.detail_img-slide').slick({
 		dots: true,
 		variableWidth: true,
 		waitForAnimate: false,
@@ -9,7 +9,7 @@ if (window.innerWidth <= 959) {
 	});
 } else {
 	//PC表示
-	$(".detail_img-slide").slick({
+	$('.detail_img-slide').slick({
 		dots: true,
 		variableWidth: true,
 		waitForAnimate: false,
@@ -18,4 +18,23 @@ if (window.innerWidth <= 959) {
 		nextArrow:
 			"<img src='../image/modal/r.png' class='slide-arrow next-arrow'>",
 	});
+}
+
+// ｢Pile｣の時のみ、スライド画像のサイズを
+// PC→縦710px,横448px
+// SP→縦300px,横185px
+// に変更する
+if (document.URL.match('Pile')) {
+	console.log(window.innerWidth);
+	if (window.innerWidth < 480) {
+		console.log('SPOK');
+		//SP表示
+		$('.detail_img-slide').css({ height: '484px', width: '300px' });
+		$('.detail_img-slide_image').css({ height: '484px', width: '300px' });
+	} else {
+		console.log('PCOK');
+		//PC,TAB表示
+		$('.detail_img-slide').css({ height: '710px', width: '448px' });
+		$('.detail_img-slide_image').css({ height: '710px', width: '448px' });
+	}
 }
